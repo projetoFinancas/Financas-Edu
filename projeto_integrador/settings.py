bce34775ecc7eaee99128dab8aad329b9f5abed3
Educa Finanças <projetofinancas034@gmail.com>
2024-12-02 00:59:49 -0300

    Update settings.py

--- projeto_integrador/settings.py
+++ projeto_integrador/settings.py
@@ -25,12 +25,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET KEY = os.environ.get("DJANGO SECRET KEY')
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG int(os.environ.get('DJANGO DEBUG', default=6))
 
-ALLOWED HOSTS = str(os.environ.get('DJANGO ALLOWED_HOSTS')).split('.')
+ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', '').split(',')
 
 
 # Application definition

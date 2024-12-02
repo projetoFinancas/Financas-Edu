abca5295a35328c83fd7aad90ebedd20812529db
Educa Finanças <projetofinancas034@gmail.com>
2024-12-01 21:10:39 -0300

    Update settings.py

--- projeto_integrador/settings.py
+++ projeto_integrador/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-9z+(o3qh3z^6ifvz4-i96@*p5i7%-*wf3!l0rugb3y4athsjt@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['vercel.app']
 
 
 # Application definition
@@ -123,6 +123,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
